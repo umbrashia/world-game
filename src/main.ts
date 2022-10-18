ff36ce5264f667ab5a9c7c1b23fbcb502dc376f2
shantanu sharma <shantanu.x.sharma@kp.org>
2022-10-19 00:54:55 +0530

    inital game setup

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { MainController } from "./application";
+
+import "./assets/css/style.css"; // import typescript Logo from './typescript.svg"
+
+document.querySelector(`#app`)?.innerHTML = `
+
+<div class="canvasDiv"> <canvas class="mainCanvas"></canvas>
+
+</div>`;
+
+//new MainController();
