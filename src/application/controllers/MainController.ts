32eb75a45c8c2d25c803230d7b08c5ccce9d781f
shantanu sharma <shantanu.x.sharma@kp.org>
2022-10-19 13:59:17 +0530

    updating small files

--- src/application/controllers/MainController.ts
+++ src/application/controllers/MainController.ts
@@ -2,9 +2,6 @@ import IMainController from "./IMainController";
 import fileImageShadowDog from "../../assets/images/shadow_dog.png";
 
 export default class MainController implements IMainController {
-  initMain() {
-    throw new Error("Method not implemented.");
-  }
   static imageShadow: HTMLImageElement | CanvasImageSource | null = null;
 
   static xMe: number = 0;
@@ -12,16 +9,12 @@ export default class MainController implements IMainController {
   public static ctxContext: CanvasRenderingContext2D | null = null;
   public static CANVAS_HEIGHT: number = 0;
   public static CANVAS_WIDTH: number = 0;
-  private canvas: HTMLCanvasElement | null = null;
+  private canvas: HTMLCanvasElement;
 
   private mainDiv: HTMLDivElement;
-
   static spriteWidth = 575;
-
   static spriteHight = 523;
-
   static frameX = 0;
-
   static frameY = 0;
 
   static gameFrame = 0;
@@ -30,8 +23,21 @@ export default class MainController implements IMainController {
   constructor() {
     this.mainDiv = document.querySelector<any>(`.canvasDiv`);
     if (!this.mainDiv) throw new Error("no html div");
-    this.canvas = null || this.canvas;
+    this.canvas = this.mainDiv
+      .getElementsByClassName(`mainCanvas`)
+      .item(0) as HTMLCanvasElement;
   }
+
+  initMain() {
+    MainController.CANVAS_WIDTH = this.canvas.width = 600;
+    MainController.CANVAS_HEIGHT = this.canvas.height = 600;
+    MainController.ctxContext = this.canvas.getContext("2d");
+    if (!MainController.ctxContext) throw new Error("2d context not found");
+    MainController.imageShadow = new Image();
+    MainController.imageShadow.src = fileImageShadowDog;
+    MainController.animate();
+  }
+
   static animate() {
     MainController.ctxContext?.clearRect(
       0,
@@ -51,16 +57,12 @@ export default class MainController implements IMainController {
       MainController.CANVAS_WIDTH,
       MainController.CANVAS_HEIGHT
     );
-
     if (MainController.gameFrame % MainController.staggerFrames == 0) {
       if (MainController.frameX <= 5) MainController.frameX++;
       else MainController.frameX = 0;
       MainController.gameFrame = 0;
     }
     MainController.gameFrame++;
-
-    // MainController.ctxContext?.fillRect (MainController.xMe, 50, 100, 100);
-
     requestAnimationFrame(MainController.animate);
   }
 }
