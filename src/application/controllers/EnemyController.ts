4befe41ebb95e09080bd91cf7a63e607c4851865
shantanu sharma <shantanu.x.sharma@kp.org>
2023-01-02 13:48:34 +0530

    adding prettier standards

--- src/application/controllers/EnemyController.ts
+++ src/application/controllers/EnemyController.ts
@@ -1,28 +1,28 @@
-import { AbstractAppClass } from "../common";
-import { IControllerBody } from "../common/IControllerBody";
+import { AbstractAppClass } from '../common'
+import { IControllerBody } from '../common/IControllerBody'
 
 export default class EnemyController
   extends AbstractAppClass
   implements IControllerBody.IControllerBody, IControllerBody.IFunctionAnimate
 {
   constructor() {
-    super();
-    super.CANVAS_HEIGHT = 1000;
-    super.CANVAS_WIDTH = 500;
+    super()
+    super.CANVAS_HEIGHT = 1000
+    super.CANVAS_WIDTH = 500
   }
 
   initMain(): void {
     try {
-      super.initMain();
+      super.initMain()
 
       //this.doAnimate();
     } catch (error) {
-      console.error(`error : `, error);
+      console.error(`error : `, error)
     }
   }
   doAnimate(): void {
-    this.ctxContext?.clearRect(0, 0, this.CANVAS_WIDTH, this.CANVAS_HEIGHT);
+    this.ctxContext?.clearRect(0, 0, this.CANVAS_WIDTH, this.CANVAS_HEIGHT)
 
-    requestAnimationFrame(() => this.doAnimate());
+    requestAnimationFrame(() => this.doAnimate())
   }
 }
